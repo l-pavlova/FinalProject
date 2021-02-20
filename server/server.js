b3807a7cc68bf1fed31a044b76a517d1cd1c3646
Lyudmila <lusito.pav@gmail.com>
2021-02-20 12:14:14 +0200

    Try depl

--- server/server.js
+++ server/server.js
@@ -52,11 +52,11 @@ io.on('connection', socket => {
 });
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'messaging-platform/build')));
+  app.use(express.static(path.join(__dirname, 'build/static')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
     console.log('it works');
-    res.sendFile(path.join(__dirname, 'messaging-platform/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build/static', 'index.html'));
   });
 }
 
